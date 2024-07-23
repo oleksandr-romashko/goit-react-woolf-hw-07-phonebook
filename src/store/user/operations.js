f6f823e3c10ec1e580d23392756303de264925c3
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-07-23 23:03:51 +0200

    Add feature delete all user data

--- src/store/user/operations.js
+++ src/store/user/operations.js
@@ -2,7 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import isEqual from 'lodash.isequal';
 
 import {
-  profileRequestStatus,
   setProfileErrorAction,
   setProfileLoadingAction,
 } from 'store/profile/slice';
